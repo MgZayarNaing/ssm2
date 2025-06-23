277707002756023ad05f9c0c2a45f7bd73a5dcb8
MyatTHiHaTun7 <myatthihatun78@gmail.com>
2025-06-23 17:32:40 +0630

    static

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
+
+class ServiceModel (models.Model):
+    name = RichTextField()
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='service',null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
