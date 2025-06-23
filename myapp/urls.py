6d9111bffe5d51cb1ee760665b7ffab22997a408
AhhBob-dot <sanwinht9t@gmail.com>
2025-06-23 22:04:36 +0630

    index3 gallery

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import index
+from .views import index3
 
 urlpatterns = [
     path('index/', index, name='index'),
-    
+    path('index3/', index3, name='index3'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -11,3 +11,12 @@ def index(request):
         'member'    :member,
     }
     return render(request, 'index.html',context)
+
+def index3(request):
+    service = ServiceModel.objects.all()
+    member  = Team_memberModel.objects.all()
+    context = {
+        'service'   :service,
+        'member'    :member,
+    }
+    return render(request, 'index3.html',context)
