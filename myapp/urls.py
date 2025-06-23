96b0be0fbebbe9468f2e68fff348b3af70025356
AhhBob-dot <sanwinht9t@gmail.com>
2025-06-23 22:16:43 +0630

    gallery

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import index
-from .views import index3
 
 urlpatterns = [
     path('index/', index, name='index'),
-    path('index3/', index3, name='index3'),
 ]
